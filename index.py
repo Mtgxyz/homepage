859f567bada3feaaf130ca57056140bfa7aaa23c
Morten Delenk <mtg.morten@googlemail.com>
2016-04-12 19:50:14 +0200

    Bugfix

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
 from htmlgen import *
+print("Content-type: text/html")
+print()
 html=htmlgen.HTMLgen(pagelayout.getLayoutXML().decode('utf-8'),"Home Page")
 html.addArticle("Markdown test","*Hello, World!*")
 print(html.renderSite())
